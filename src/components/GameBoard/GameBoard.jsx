a5866af19bcebe48f3855fb1639fc3cc85feda6c
Heath Attig <heath.attig@gmail.com>
2018-07-29 06:47:29 -0500

    add title to GameBoard

--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import './GameBoard.css'
 
 export default class GameBoard extends React.Component {
   render() {
     return (
       <div className='GameBoard'>
-        Forty Thieves
+        {this.props.title}
       </div>
     )
   }
+}
+
+GameBoard.propTypes = {
+  title: PropTypes.string.isRequired
 }
--- src/components/GameBoard/GameBoard.test.js
+++ src/components/GameBoard/GameBoard.test.js
@@ -4,6 +4,6 @@ import GameBoard from './GameBoard';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<GameBoard />, div);
+  ReactDOM.render(<GameBoard title="Forty Thieves"/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
